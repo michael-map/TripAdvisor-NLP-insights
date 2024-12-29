7df3bcf1196be4d7b61784d2ee541515b5035e32
michael-map <michael.map90@proton.me>
2024-12-29 23:07:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import joblib
 from utils import preprocessor
 
 from huggingface_hub import hf_hub_download
@@ -8,7 +7,6 @@ import joblib
 
 REPO_ID = "michael-map/tripadvisor-nlp-rfc"
 FILENAME = "random_forest_model.joblib"
-
 
 def run():
     model = joblib.load(hf_hub_download(repo_id=REPO_ID, filename=FILENAME))
